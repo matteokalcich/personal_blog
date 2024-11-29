e0708d995c7e5c8f66e1f0963ed0268447e69de0
matteo <matteokalcich@gmail.com>
2024-11-29 11:52:56 +0100

    todo: add allb and chasnge btn

---
+++ dettagliPost.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dettagli Post</title>
+</head>
+<body>
+    
+
+    <div class="primaParte">
+                <img id="logo" src="./assets/images/react_logo.png" alt="React logo">
+                <div class="titolo">
+                    <h1>Blog Personale Matteo Kalcich</h1>
+                </div>
+    </div>
+
+    <div class="secondaParte">
+
+        <button id="loginBtn" name="login" type="button"><a href="./login.php">Login</a></button>
+    </div>
+</body>
+</html>
